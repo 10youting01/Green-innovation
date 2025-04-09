7df2a1a6495977c29d2d16068a4ee3281a9d8fe2
10youting01 <10youting01@gmail.com>
2025-04-09 16:47:35 +0800

    (Model1) Refactor to enhance directory structure for versioning and results storage

--- greenwashing_prediction/models/model1_without_handmade_features.py
+++ greenwashing_prediction/models/model1_without_handmade_features.py
@@ -5,7 +5,16 @@ from sklearn.model_selection import TimeSeriesSplit
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import get_X_y_from_dataset_without_handmade_features, train_one_fold, evaluate_model, predict, save_model, save_predictions, save_metrics_log
-from models.mlp import build_mlp1_model
+from models.mlp import build_mlp_model
+
+## Define version
+version = "v0"  # Adjust this for each new version of the model
+
+# Create directories for the current version
+model_folder = f"../models/model1/model1_{version}"
+result_folder = f"../results/model1_{version}"
+os.makedirs(model_folder, exist_ok=True)
+os.makedirs(result_folder, exist_ok=True)
 
 ## Read and check data
 csr_embeddings_score = pd.read_csv("../data/csr_embeddings_score.csv")
@@ -19,48 +28,54 @@ csr_embeddings_score.sort_values(
     inplace=True,
 )
 X, y = get_X_y_from_dataset_without_handmade_features(csr_embeddings_score)
-## Check the shape of the data
-# print(f"X shape: {X.shape}")
-# print(f"y shape: {y.shape}")
 
 ## Time Series K-Fold
 tscv = TimeSeriesSplit(n_splits=5)
 metrics_all_folds = []
+
 for fold, (train_index, test_index) in enumerate(tscv.split(X)):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
     ## Build the model
-    model = build_mlp1_model()   
+    model = build_mlp_model("mlp1")   
     ## Train the model
     model = train_one_fold(model, X_train, y_train, X_test, y_test, lr=0.005, max_epochs=500, early_stop_rounds=10)
+    
     ## Predict the model
     y_pred = predict(model, X_test)
+    
     ## Evaluate the model
     metrics = evaluate_model(y_test, y_pred)
-    # print(f"Fold {fold+1} Metrics: {metrics}")
+    print(f"Fold {fold+1} Metrics: {metrics}")
+    print(f"Training samples: {len(X_train)}, Test samples: {len(X_test)}")
     metrics["fold"] = fold + 1
     metrics_all_folds.append(metrics)
-    ## Print the metrics
-    print(f"Metrics: {metrics}")
-    ## Save the model
 
-        # Save model
-#     save_model(model, f"../models/DNN_model1/fold{fold+1}_model.pkl")
+    ## Save model for this version
+    model_path = os.path.join(model_folder, f"fold{fold+1}_model_{version}.pkl")
+    save_model(model, model_path)
 
-#     # Save predictions
-#     save_predictions(y_test, y_pred, f"../results/DNN_model1/fold{fold+1}_predictions.csv")
+    ## Save predictions for this version
+    predictions_path = os.path.join(result_folder, f"fold{fold+1}_predictions_{version}.csv")
+    save_predictions(y_test, y_pred, predictions_path)
 
-# # Save metrics log (after all folds)
-# save_metrics_log(metrics_all_folds, "../results/DNN_model1/metrics_log.json")
+# Save metrics log (after all folds) for this version
+save_metrics_log(metrics_all_folds, f"../results/model1_{version}/metrics_log_{version}.json")
 
-# Early stopping at epoch 22
-# Metrics: {'MSE': 253.17392560138035, 'RMSE': 15.911440085717583, 'MAE': 11.946565503480114, 'MAPE': 4.649468814641202, 'fold': 1}
+
+# Early stopping at epoch 21
+# Fold 1 Metrics: {'MSE': 242.69813545673793, 'RMSE': 15.578771949570926, 'MAE': 11.710377014265582, 'MAPE': 4.390193185648197}
+# Training samples: 823, Test samples: 823
+# Early stopping at epoch 23
+# Fold 2 Metrics: {'MSE': 256.0272353143279, 'RMSE': 16.00085108093716, 'MAE': 11.685437623310131, 'MAPE': 6.375378498762015}
+# Training samples: 1646, Test samples: 823
+# Early stopping at epoch 19
+# Fold 3 Metrics: {'MSE': 217.2693649877037, 'RMSE': 14.740059870560353, 'MAE': 10.92326680849885, 'MAPE': 4.057159371210535}
+# Training samples: 2469, Test samples: 823
+# Early stopping at epoch 25
+# Fold 4 Metrics: {'MSE': 243.21495943496464, 'RMSE': 15.595350571082545, 'MAE': 11.193176979844317, 'MAPE': 2.9576815121995814}
+# Training samples: 3292, Test samples: 823
 # Early stopping at epoch 18
-# Metrics: {'MSE': 237.55195562254144, 'RMSE': 15.412720578228278, 'MAE': 11.381758628320124, 'MAPE': 5.238755290973708, 'fold': 2}
-# Early stopping at epoch 22
-# Metrics: {'MSE': 223.11486633941763, 'RMSE': 14.9370300374411, 'MAE': 11.042352206132934, 'MAPE': 4.54240239615341, 'fold': 3}
-# Early stopping at epoch 24
-# Metrics: {'MSE': 238.83837390079728, 'RMSE': 15.454396588052129, 'MAE': 11.128429970971146, 'MAPE': 2.805332797283217, 'fold': 4}
-# Early stopping at epoch 31
-# Metrics: {'MSE': 222.67165184184634, 'RMSE': 14.922186563699247, 'MAE': 10.904291063073458, 'MAPE': 3.6309857519490154, 'fold': 5}
+# Fold 5 Metrics: {'MSE': 229.7444763057485, 'RMSE': 15.157324180268379, 'MAE': 11.074404233412936, 'MAPE': 4.053625243786816}
+# Training samples: 4115, Test samples: 823
